79a6c5484a15a1150cdd6235c7b59f91d2f6c654
Rafa-Oli <rafaela.albaniza@gmail.com>
2020-10-23 20:57:23 -0300

    android

--- app/src/main/java/com/rafa/convidados/service/model/GuestModel.kt
+++ app/src/main/java/com/rafa/convidados/service/model/GuestModel.kt
@@ -1,4 +1,21 @@
 package com.rafa.convidados.service.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //mapeamentos de dados, atributos
-data class GuestModel(val id :Int = 0, var name: String, var presence: Boolean)
+//entidade
+@Entity(tableName= "Guest")
+class GuestModel{
+    @PrimaryKey(autoGenerate= true)
+    @ColumnInfo(name = "id")
+    var id: Int =0
+
+    @ColumnInfo(name = "name")
+    var name: String= ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean= true
+}
 
